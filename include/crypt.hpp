b59eced07d1c0c36c349b8fcebaade0fd7994d77
Will Oliver <willoliver.main@gmail.com>
2024-01-01 21:57:04 +0000

    first commit

---
+++ include/crypt.hpp
@@ -0,0 +1,44 @@
+#ifndef INCLUDE_CRYPT_H
+#define INCLUDE_CRYPT_H
+
+#include "cipher.hpp"
+#include "cipher_context.hpp"
+
+namespace crpt {
+
+/**
+ * @brief Provides OpenSSL encryption/decryption
+ */
+class Crypt {
+    private:
+        CipherContext context;
+        Cipher cipher;
+
+    public:
+        /**
+         * @brief Construct a new Crypt object
+         * 
+         * @tparam Args Should be synonymous to some Cipher constructor argument list, minus the leading EVP_CIPHER_CTX* arugment.
+         * @param args See CipherContext::new_cipher(Args&&... args).
+         */
+        template <typename... Args>
+        Crypt(Args&&... args):
+            context {},
+            cipher { context.new_cipher(args...) }
+        {}
+
+        inline bool context_is_null() const {
+            return context.is_null();
+        }
+
+        inline bool cipher_is_null() const {
+            return cipher.is_null();
+        }
+
+        std::pair<std::string, bool> encrypt(std::string plain_text, std::string key, std::string iv);
+        std::pair<std::string, bool> decrypt(std::string cipher_text, std::string key, std::string iv);
+};
+
+}
+
+#endif
---
+++ test/crypt.cpp
@@ -0,0 +1,51 @@
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include "crypt.hpp"
+
+using namespace crpt;
+
+char key_256[] = { 
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+};
+
+char iv_128[] = { 
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
+};
+
+BOOST_AUTO_TEST_CASE( crypt_constructor_test ) {
+    Crypt aes { "AES-256-CBC" };
+    BOOST_ASSERT( !aes.context_is_null() );
+    BOOST_ASSERT( !aes.cipher_is_null() );
+
+    Crypt not_aes { "NOT-A-REAL-ALG" };
+    BOOST_ASSERT( not_aes.cipher_is_null() );
+}
+
+BOOST_AUTO_TEST_CASE( crypt_simple_encrypt_test ) {
+    Crypt aes { "AES-256-CBC" };
+
+    std::string plain_text { "Hello, World!" };
+    auto [cipher_text, success] = aes.encrypt(plain_text, key_256, iv_128);
+
+    BOOST_ASSERT( success );
+    BOOST_ASSERT( cipher_text != plain_text );
+}
+
+BOOST_AUTO_TEST_CASE( crypt_simple_decrypt_test ) {
+    Crypt aes { "AES-256-CBC" };
+
+    std::string plain_text = { "Hello, World!" };
+    auto [cipher_text, success] = aes.encrypt(plain_text, key_256, iv_128);
+    
+    BOOST_ASSERT( success );
+    BOOST_ASSERT( cipher_text != plain_text );
+
+    auto [decrypted, decrypt_success] = aes.decrypt(cipher_text, key_256, iv_128);
+
+    BOOST_ASSERT( decrypt_success );
+    BOOST_ASSERT( plain_text == decrypted );
+}
